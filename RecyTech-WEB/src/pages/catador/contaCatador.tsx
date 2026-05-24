2516b8274b9b426ea4dcf5558da92e3b30739e05
ShavedGhost <shavedghost@hotmail.com>
2026-05-24 13:10:28 -0300

    separei o css do contaCatador

--- RecyTech-WEB/src/pages/catador/contaCatador.tsx
+++ RecyTech-WEB/src/pages/catador/contaCatador.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Sidebar from "../../components/Sidebar.tsx";
 import { routesMapCatador } from "../../routes/routesMap.ts";
-import "../../style/cidadao/conta.css";
+import "../../style/catador/contaCatador.css";
 import { catadorService } from "../../services/catador/catadorService.ts";
 import { enderecoService } from "../../services/cidadao/enderecoService.ts";
 import type { Endereco, EnderecoInput } from "../../types/types.ts";
